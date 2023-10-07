e6ff695ccfc2ce60588dc04628502ca031b90697
unknownrocks619 <unknownrocks619@hotmail.com>
2023-10-07 20:44:58 +0100

    Revert "nature-theme"
    This reverts commit da17d696dee481a705cdc41bc7a2f5e3c3e731cb.

--- routes/backend/header.php
+++ routes/backend/header.php
@@ -5,5 +5,4 @@ Route::prefix('themes')
         ->group(function() {
             Route::match(['get','post'],'header/list',[\App\Http\Controllers\Admin\Themes\HeaderController::class,'header'])->name('header.list');
             Route::match(['get','post'],'footer/list',[\App\Http\Controllers\Admin\Themes\HeaderController::class,'footer'])->name('footer.list');
-
         });
