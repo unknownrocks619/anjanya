7d5c91fe902e72b64ed4d80fd947ae6038dfa279
unknownrocks619 <unknownrocks619@hotmail.com>
2023-10-27 20:17:49 +0100

    block all access

--- routes/frontend/frontend.php
+++ routes/frontend/frontend.php
@@ -20,39 +20,42 @@ Route::middleware(['web', 'maintenance'])
             return view('maintenance.index');
         })->name('maintenance-mode');
 
-        Route::get('/', [MenuController::class, 'load'])->name('home');
-        Route::post('/contact-submission-form', [MenuController::class, 'submit_contact_us'])->name('submit_contanct_us');
+        Route::get('/',function(){
+            return view('maintenance.index');
+        })->name('home');
+//        Route::get('/', [MenuController::class, 'load'])->name('home');
+//        Route::post('/contact-submission-form', [MenuController::class, 'submit_contact_us'])->name('submit_contanct_us');
 
         /**
          * Static
          */
-        Route::get('/report-a-problem', function () {
-            return view('frontend.report');
-        });
+//        Route::get('/report-a-problem', function () {
+//            return view('frontend.report');
+//        });
 
         /**
          * News letter
          */
-        include __DIR__ . '/newsletter.php';
-
-        /**
-         * Profile
-         */
-        include __DIR__ . '/profile.php';
-
-        /**
-         * Users
-         */
-
-        include __DIR__ . '/users.php';
-
-        /**
-         * Pages
-         */
-        include __DIR__ . '/pages.php';
-
-        /**
-         * Category
-         */
-        include __DIR__ . '/category.php';
+//        include __DIR__ . '/newsletter.php';
+//
+//        /**
+//         * Profile
+//         */
+//        include __DIR__ . '/profile.php';
+//
+//        /**
+//         * Users
+//         */
+//
+//        include __DIR__ . '/users.php';
+//
+//        /**
+//         * Pages
+//         */
+//        include __DIR__ . '/pages.php';
+//
+//        /**
+//         * Category
+//         */
+//        include __DIR__ . '/category.php';
     });
