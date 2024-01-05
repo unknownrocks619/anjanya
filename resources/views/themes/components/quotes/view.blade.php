fecf5fe875fb551577d6ede8b8bfb6202b95feca
binodgiri <unknownrocks619@hotmail.com>
2024-01-05 14:37:42 +0545

    tinymce link

--- resources/views/themes/components/quotes/view.blade.php
+++ resources/views/themes/components/quotes/view.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <figure>
         <blockquote class="blockquote">
-            <p>{{$componentValue['quote']}}</p>
+            <p style="border-left: 2px solid;padding:17px;" class="border-success">{{$componentValue['quote']}}</p>
         </blockquote>
         {{--    <figcaption class="blockquote-footer">--}}
         {{--        Someone famous in <cite title="Source Title">Source Title</cite>--}}
