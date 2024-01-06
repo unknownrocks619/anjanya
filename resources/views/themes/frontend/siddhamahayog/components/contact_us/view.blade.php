dda9038cc3a2b4b51ca979566e17db67bb7462a3
binodgiri <unknownrocks619@hotmail.com>
2024-01-06 16:35:29 +0545

    primary email

--- resources/views/themes/frontend/siddhamahayog/components/contact_us/view.blade.php
+++ resources/views/themes/frontend/siddhamahayog/components/contact_us/view.blade.php
@@ -65,7 +65,7 @@
                                     </div>
                                     <div class="content">
                                         <h6 class="title">Email Us</h6>
-                                        <p><a href="mailto:{{\App\Classes\Helpers\SystemSetting::primary_contact_info('official_email')}}" target="_blank">{{\App\Classes\Helpers\SystemSetting::primary_contact_info('official_email')}}</a></p>
+                                        <p><a href="mailto:{{\App\Classes\Helpers\SystemSetting::basic_configuration('official_email')}}" target="_blank">{{\App\Classes\Helpers\SystemSetting::basic_configuration('official_email')}}</a></p>
                                     </div>
                                 </div>
                             </div>
