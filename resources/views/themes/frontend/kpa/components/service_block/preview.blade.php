c94089ba027548f76c64a04fd1649d90008aebf5
binodgiri <unknownrocks619@hotmail.com>
2024-06-10 00:26:09 +0100

    kpa service block

---
+++ resources/views/themes/frontend/kpa/components/service_block/preview.blade.php
@@ -0,0 +1,167 @@
+
+@php
+$title = isset($title) ? 'Most Recent <span>' . $title .'</span>' : 'Blog Post';
+$subtitle = isset($subtitle) ? $subtitle : 'Blog Updates';
+$description = isset($description)  ? $description : '';
+$posts = isset($posts) ? $posts : [];
+
+@endphp
+
+@extends('themes.frontend.kpa.layout.preview-layout')
+@section('main')
+<div id="commonComponentBuilder">
+    <!-- rts service post area  Start-->
+    <div class="rts-service-area rts-section-gap">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="rts-title-area service text-center">
+                        <p class="pre-title">
+                            Our Services
+                        </p>
+                        <h2 class="title">High Quality Services</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid service-main plr--120-service mt--50 plr_md--0 pl_sm--0 pr_sm--0">
+            <div class="background-service row">
+                <!-- start single Service -->
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="service-one-inner one">
+                        <div class="thumbnail">
+                            <img src="assets/images/service/icon/01.svg" alt="finbiz_service">
+                        </div>
+                        <div class="service-details">
+                            <a href="service-details.html">
+                                <h5 class="title">Business Planning</h5>
+                            </a>
+                            <p class="disc">
+                                Sagitis himos pulvinar morb socis laoreet posuere enim non auctor etiam pretium libero
+                            </p>
+                            <a class="rts-read-more btn-primary" href="service-details.html"><i
+                                    class="far fa-arrow-right"></i>Read
+                                More</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single Services -->
+                <!-- start single Service -->
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="service-one-inner two">
+                        <div class="thumbnail">
+                            <img src="assets/images/service/icon/02.svg" alt="finbiz_service">
+                        </div>
+                        <div class="service-details">
+                            <a href="service-details.html">
+                                <h5 class="title">Develop Process</h5>
+                            </a>
+                            <p class="disc">
+                                Sagitis himos pulvinar morb socis laoreet posuere enim non auctor etiam pretium libero
+                            </p>
+                            <a class="rts-read-more btn-primary" href="service-details.html"><i
+                                    class="far fa-arrow-right"></i>Read
+                                More</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single Services -->
+                <!-- start single Service -->
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="service-one-inner three">
+                        <div class="thumbnail">
+                            <img src="assets/images/service/icon/03.svg" alt="finbiz_service">
+                        </div>
+                        <div class="service-details">
+                            <a href="service-details.html">
+                                <h5 class="title">Strategy & Planning</h5>
+                            </a>
+                            <p class="disc">
+                                Sagitis himos pulvinar morb socis laoreet posuere enim non auctor etiam pretium libero
+                            </p>
+                            <a class="rts-read-more btn-primary" href="service-details.html"><i
+                                    class="far fa-arrow-right"></i>Read
+                                More</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single Services -->
+                <!-- start single Service -->
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="service-one-inner four">
+                        <div class="thumbnail">
+                            <img src="assets/images/service/icon/04.svg" alt="finbiz_service">
+                        </div>
+                        <div class="service-details">
+                            <a href="service-details.html">
+                                <h5 class="title">Business Support</h5>
+                            </a>
+                            <p class="disc">
+                                Sagitis himos pulvinar morb socis laoreet posuere enim non auctor etiam pretium libero
+                            </p>
+                            <a class="rts-read-more btn-primary" href="service-details.html"><i
+                                    class="far fa-arrow-right"></i>Read
+                                More</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single Services -->
+                <!-- start single Service -->
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="service-one-inner five">
+                        <div class="thumbnail">
+                            <img src="assets/images/service/icon/05.svg" alt="finbiz_service">
+                        </div>
+                        <div class="service-details">
+                            <a href="service-details.html">
+                                <h5 class="title">Audit & Evaluation</h5>
+                            </a>
+                            <p class="disc">
+                                Sagitis himos pulvinar morb socis laoreet posuere enim non auctor etiam pretium libero
+                            </p>
+                            <a class="rts-read-more btn-primary" href="service-details.html"><i
+                                    class="far fa-arrow-right"></i>Read
+                                More</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single Services -->
+                <!-- start single Service -->
+                <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div class="service-one-inner six">
+                        <div class="thumbnail">
+                            <img src="assets/images/service/icon/06.svg" alt="finbiz_service">
+                        </div>
+                        <div class="service-details">
+                            <a href="service-details.html">
+                                <h5 class="title">Consultancy & Advice</h5>
+                            </a>
+                            <p class="disc">
+                                Sagitis himos pulvinar morb socis laoreet posuere enim non auctor etiam pretium libero
+                            </p>
+                            <a class="rts-read-more btn-primary" href="service-details.html"><i
+                                    class="far fa-arrow-right"></i>Read
+                                More</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single Services -->
+            </div>
+            <div class="row">
+                <div class="cta-one-bg col-12">
+                    <div class="cta-one-inner">
+                        <div class="cta-left">
+                            <h3 class="title">Let’s discuss about how we can help
+                                make your business better</h3>
+                        </div>
+                        <div class="cta-right">
+                            <a class="rts-btn btn-white" href="appoinment.html">Lets Work Together</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- rts service post area ENd -->
+@endsection
