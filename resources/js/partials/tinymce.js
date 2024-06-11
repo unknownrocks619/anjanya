f7af47a1866d7c669fc8a3da26e09cb4409b8564
binodgiri <unknownrocks619@hotmail.com>
2024-06-11 20:26:32 +0100

    kpa

--- resources/js/partials/tinymce.js
+++ resources/js/partials/tinymce.js
@@ -8,10 +8,22 @@ $(function () {
     window.setupTinyMce = function () {
         tinymce.init({
             selector: 'textarea',
-            plugins: ' anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount,accordion,fullscreen,quickbars,advlist',
-            toolbar: 'undo redo fullscreen| blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight  | numlist bullist indent outdent | emoticons charmap | removeformat accordion',
-            fullscreen_native : true,
+            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            tinycomments_mode: 'embedded',
+            tinycomments_author: 'Author name',
+            mergetags_list: [
+                { value: 'First.Name', title: 'First Name' },
+                { value: 'Email', title: 'Email' },
+            ],
+            // ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
         });
+        // tinymce.init({
+        //     selector: 'textarea',
+        //     plugins: ' anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount,accordion,fullscreen,quickbars,advlist',
+        //     toolbar: 'undo redo fullscreen| blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight  | numlist bullist indent outdent | emoticons charmap | removeformat accordion',
+        //     fullscreen_native : true,
+        // });
     }
 
 window.setupTinyMceAll = function () {
